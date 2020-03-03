b9c71b0ffb71fe8f4fd5f79bf9dacbf6d09c581f
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-03-03 17:09:04 +0300

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    let doubleLetterArr = expr.match(/.{1,2}/g);
+    let arr = [];
+    doubleLetterArr.forEach(function(elem) {
+        if ( elem == "00") {
+            arr.push("f")
+        } else if (elem == "10") {
+            arr.push(".");
+        } else if (elem == "11") {
+            arr.push("-");
+        } else if (elem == "**") {
+            arr.push(" "); 
+        }
+    });
+    let morseString = arr.join("");
+    let fiveLetterArr = morseString.match(/.{1,5}/g);
+    let result = "";
+
+    let morseArr = fiveLetterArr.map(function(item) {
+        return item.replace(/f/g,'').trim();
+    })
+
+    morseArr.forEach(function(item) {
+        if (item.length == 0) {
+            result += " ";
+        }
+        for (let key of Object.keys(MORSE_TABLE)) {
+            if (item == key) {
+                result += MORSE_TABLE[key];
+            }              
+        }
+    });
+    return result;
 }
 
 module.exports = {
